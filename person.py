e46a2c54501f4369d5ba726bfeacc55326d944b1
Morrison Yasmin <morrisonismailayasmin@gmail.com>
2024-08-21 17:00:38 +0100

    Creating the person class

---
+++ person.py
@@ -0,0 +1,11 @@
+def get_age():
+    age = int(input("Enter your age: "))
+    print(age)
+    print(type(age))
+get_age()
+
+def get_name():
+    name= (input("Enter your name: "))
+    print(name)
+    print(type(name))
+get_name()
