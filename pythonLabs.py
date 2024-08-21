8ce4aebc132e660ecfd5e2575a39580875daba03
Morrison Yasmin <morrisonismailayasmin@gmail.com>
2024-08-21 16:20:00 +0100

    basic python - first part of python labs

---
+++ pythonLabs.py
@@ -0,0 +1,36 @@
+#basic python
+print("hello") # it prints hello to the terminal
+print('hello') # it also prints hello to the terminal
+#print(hello) # it gives an error
+
+#math
+print(4 + 5)
+
+print (8 * 3)
+
+print (4 /3)
+
+print(4.0 / 3)
+
+print(3 +5 *9)
+
+#adding strings
+print("abc" + "def")
+print ("hello" + " " + "world")
+print("123" + "5")
+
+#variables
+first = "hello"
+second = "world"
+third = first + " " + second
+print(type(first))
+print(first)
+print (third)
+
+#user input
+name = input("what is your name?")
+print(type(name))
+age = input("What is your age?")
+print(type(age))
+
+#CONVERSIONS
