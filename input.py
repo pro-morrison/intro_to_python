3f9feb46359e08edd3b3b1c839dda6b7a55f491a
Morrison Yasmin <morrisonismailayasmin@gmail.com>
2024-08-21 13:10:53 +0100

    Class practiced programs

---
+++ input.py
@@ -0,0 +1,27 @@
+# This program takes a two digit number from the user and adds the two numbers together
+"""
+number = input("Enter a two digit number: ")
+num1 = number[0] 
+num2 = number[1]
+result = int(num1) + int(num2)
+print(f"The sum of the two digits is {result}")"""
+
+
+# Calculate body mass index
+"""weight = input ("Enter your weight in Kg: ")
+height = input ("Enter your height in meter: ")
+actualWeight = int(weight)
+actualHeight = int(height)
+bmi = actualWeight/(actualHeight*actualHeight)
+
+print (f"Your bmi is {bmi}")"""
+
+#conditional statements
+# allow your use to enter a number and tell them if its an even or odd number
+
+number = input("Enter a number: ")
+num = int(number)
+if (num%2 ==0):
+    print("You have an even number")
+else:
+     print("You have an oddd number")
